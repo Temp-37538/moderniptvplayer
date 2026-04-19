e46520c6e02c960e43e58004b75f2fc222e10d14
Temp-37538 <193842841+Temp-37538@users.noreply.github.com>
2026-04-20 01:55:00 +0200

    feat: update emmet settings and reset secrets

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -5,12 +5,16 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql",
-  }),
-  trustedOrigins: [env.CORS_ORIGIN],
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [nextCookies()],
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	secrets: [
+		{ version: 2, value: env.BETTER_AUTH_SECRET2 },
+		{ version: 1, value: env.BETTER_AUTH_SECRET },
+	],
+	trustedOrigins: [env.CORS_ORIGIN],
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [nextCookies()],
 });
